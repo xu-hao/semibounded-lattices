1f507324b2534b6eb473df4ce7619405f434e044
Hao Xu <xuh@email.unc.edu>
2016-06-30 17:27:35 -0400

    add Heyting algebra, bi-Heyting algebra and make Boolean algebra a subclass of bi-Heyting algebra

--- src/Algebra/SemiBoundedLattice.hs
+++ src/Algebra/SemiBoundedLattice.hs
@@ -28,35 +28,32 @@ class LowerBoundedDistributiveLattice a => SemiCoHeytingAlgebra a where
   (\\\) = subtraction
   subtraction = (\\\)
 
+-- | supplement
+-- see <https://ncatlab.org/nlab/show/co-Heyting+negation>
+class (BoundedLattice a, SemiCoHeytingAlgebra a) => CoHeytingAlgebra a where
+  supplement :: a -> a
+  supplement a = top \\\ a
+
+-- | A Heyting Algebra requires a bounded lattice.
+-- here the lattice must be lower bounded
+class UpperBoundedDistributiveLattice a => SemiHeytingAlgebra a where
+  implication :: a -> a -> a
+  (-->) :: a -> a -> a
+  (-->) = implication
+  implication = (-->)
+
+-- | supplement
+-- see <https://ncatlab.org/nlab/show/co-Heyting+negation>
+class (BoundedLattice a, SemiHeytingAlgebra a) => HeytingAlgebra a where
+  negation :: a -> a
+  negation a = a --> bottom
+
+class (HeytingAlgebra a, CoHeytingAlgebra a) => BiHeytingAlgebra a where
+
 -- | A Boolean Algebra is a complemented distributive lattice, see <https://en.wikipedia.org/wiki/Boolean_algebra_(structure)>
-class (BoundedLattice a, DistributiveLattice a) => BooleanAlgebra a where
+class BiHeytingAlgebra a => BooleanAlgebra a where
   complement :: a -> a
-
-newtype BA_SCHA_Inj a = BA_SCHA_Inj a
-
-instance BooleanAlgebra a => MeetSemiLattice (BA_SCHA_Inj a) where
-  (BA_SCHA_Inj a) /\ (BA_SCHA_Inj b) = BA_SCHA_Inj (a /\ b)
-
-instance BooleanAlgebra a => JoinSemiLattice (BA_SCHA_Inj a) where
-  (BA_SCHA_Inj a) \/ (BA_SCHA_Inj b) = BA_SCHA_Inj (a \/ b)
-
-instance BooleanAlgebra a => BoundedJoinSemiLattice (BA_SCHA_Inj a) where
-  bottom = BA_SCHA_Inj bottom
-
-instance BooleanAlgebra a => BoundedMeetSemiLattice (BA_SCHA_Inj a) where
-  top = BA_SCHA_Inj top
-
-instance BooleanAlgebra a => LowerBoundedLattice (BA_SCHA_Inj a) where
-instance BooleanAlgebra a => LowerBoundedDistributiveLattice (BA_SCHA_Inj a) where
-instance BooleanAlgebra a => Lattice (BA_SCHA_Inj a) where
-instance BooleanAlgebra a => DistributiveLattice (BA_SCHA_Inj a) where
-instance BooleanAlgebra a => BoundedLattice (BA_SCHA_Inj a) where
-
-instance BooleanAlgebra a => BooleanAlgebra (BA_SCHA_Inj a) where
-    complement (BA_SCHA_Inj s) = BA_SCHA_Inj (complement s)
-
-instance BooleanAlgebra a => SemiCoHeytingAlgebra (BA_SCHA_Inj a) where
-    subtraction s1 s2 = s1 /\ complement s2
+  complement = supplement
 
 data Complemented a = Include a | Exclude a deriving Show
 
@@ -72,10 +69,6 @@ instance SemiCoHeytingAlgebra a => MeetSemiLattice (Complemented a) where
     meet (Include vars) (Exclude vars2) = Include (vars \\\ vars2)
     meet (Exclude vars) (Exclude vars2) = Exclude (vars \/ vars2)
 
-instance SemiCoHeytingAlgebra a => BooleanAlgebra (Complemented a) where
-    complement (Include vars) = Exclude vars
-    complement (Exclude vars) = Include vars
-
 instance SemiCoHeytingAlgebra a => BoundedJoinSemiLattice (Complemented a) where
     bottom = bottom
 
@@ -83,12 +76,25 @@ instance SemiCoHeytingAlgebra a => BoundedMeetSemiLattice (Complemented a) where
     top = Exclude bottom
 
 instance SemiCoHeytingAlgebra a => Lattice (Complemented a) where
-instance SemiCoHeytingAlgebra a => BoundedLattice (Complemented a) where
 instance SemiCoHeytingAlgebra a => LowerBoundedLattice (Complemented a) where
+instance SemiCoHeytingAlgebra a => UpperBoundedLattice (Complemented a) where
+instance SemiCoHeytingAlgebra a => BoundedLattice (Complemented a) where
 instance SemiCoHeytingAlgebra a => DistributiveLattice (Complemented a) where
 instance SemiCoHeytingAlgebra a => LowerBoundedDistributiveLattice (Complemented a) where
+instance SemiCoHeytingAlgebra a => UpperBoundedDistributiveLattice (Complemented a) where
 instance SemiCoHeytingAlgebra a => SemiCoHeytingAlgebra (Complemented a) where
-    a \\\ b = case BA_SCHA_Inj a \\\ BA_SCHA_Inj b of BA_SCHA_Inj c -> c
+  Include a \\\ Include b = Include (a \\\ b)
+  Include a \\\ Exclude b = Include (a /\ b)
+  Exclude a \\\ Include b = Exclude (a \/ b)
+  Exclude a \\\ Exclude b = Include (b \\\ a)
+
+instance SemiCoHeytingAlgebra a => SemiHeytingAlgebra (Complemented a) where
+  a --> b = (top \\\ a) \/ b
+
+instance SemiCoHeytingAlgebra a => CoHeytingAlgebra (Complemented a) where
+instance SemiCoHeytingAlgebra a => HeytingAlgebra (Complemented a) where
+instance SemiCoHeytingAlgebra a => BiHeytingAlgebra (Complemented a) where
+instance SemiCoHeytingAlgebra a => BooleanAlgebra (Complemented a) where
 
 instance Eq a => JoinSemiLattice [a] where
   join = union
